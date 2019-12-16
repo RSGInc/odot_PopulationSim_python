3e5d2caeb00017874d577ee79e04f3376a033829
Ben Stabler <bstabler@users.noreply.github.com>
2019-12-16 13:09:45 -0800

    Update build_populationsim_python.bat

--- build_populationsim_python.bat
+++ build_populationsim_python.bat
@@ -2,4 +2,4 @@ conda create -n popsim python=2.7
 activate popsim
 conda install cytoolz numpy pandas psutil pytables future
 pip install activitysim
-pip install https://github.com/RSGInc/populationsim/zipball/master
+pip install https://github.com/activitysim/populationsim/zipball/master
